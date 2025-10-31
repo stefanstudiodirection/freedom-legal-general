a8b90000342b59d0100fa991c0b24bf1f0ae642c
stefanstudiodirection <stefan.radovic@studiodirection.com>
2025-10-31 15:14:27 +0100

    pie chart

--- src/pages/Budgeting.tsx
+++ src/pages/Budgeting.tsx
@@ -327,7 +327,7 @@ const Budgeting: React.FC = () => {
 								data={chartData}
 								dataKey="amount"
 								nameKey="category"
-								innerRadius={60}
+								innerRadius={80}
 								strokeWidth={3}
 							>
 								<Label
